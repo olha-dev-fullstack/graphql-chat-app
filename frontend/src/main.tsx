2b3b07c29adf3290429b29c4e307526ca584af42
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-12-25 13:50:11 +0200

    feat/users real time chat finished

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apolloClient.ts";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import { MantineProvider } from "@mantine/core";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/chatrooms/:id",
+      },
+    ],
+  },
+])
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
       <ApolloProvider client={client}>
         <RouterProvider router={router} />
-        <App />
       </ApolloProvider>
     </MantineProvider>
   </StrictMode>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,25 +1,33 @@
-import AddChatroom from "../components/AddChatroom";
-import AuthOverlay from "../components/AuthOverlay";
-import ProfileSettings from "../components/ProfileSettings";
-import ProtectedRoutes from "../components/ProtectedRoutes";
-import RoomList from "../components/RoomList";
-import Sidebar from "../components/Sidebar";
-import MainLayout from "../layouts/MainLayout";
-
+import MainLayout from "../layouts/MainLayout"
+import Sidebar from "../components/Sidebar"
+import ProtectedRoutes from "../components/ProtectedRoutes"
+import AuthOverlay from "../components/AuthOverlay"
+import ProfileSettings from "../components/ProfileSettings"
+import RoomList from "../components/RoomList"
+import { Flex } from "@mantine/core"
+import AddChatroom from "../components/AddChatroom"
+import JoinRoomOrChatwindow from "../components/JoinRoomOrChatwindow"
 function Home() {
   return (
     <MainLayout>
-      <>
+      <div
+        style={{
+          position: "absolute",
+        }}
+      >
         <AuthOverlay />
-        <Sidebar />
         <ProfileSettings />
+        <Sidebar />
         <ProtectedRoutes>
-          <RoomList />
           <AddChatroom />
+          <Flex direction={{ base: "column", md: "row" }}>
+            <RoomList />
+            <JoinRoomOrChatwindow />
+          </Flex>
         </ProtectedRoutes>
-      </>
+      </div>
     </MainLayout>
-  );
+  )
 }
 
-export default Home;
+export default Home
